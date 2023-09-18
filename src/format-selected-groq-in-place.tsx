e8b1c891d71b6c12ee8db8e156a464114e366c6e
Ash <ashley@juice49.com>
2023-09-18 19:57:07 +0100

    feat: report GROQ error or success when formatting in place

--- src/format-selected-groq-in-place.tsx
+++ src/format-selected-groq-in-place.tsx
@@ -1,4 +1,4 @@
-import { Clipboard, getSelectedText } from '@raycast/api'
+import { Clipboard, getSelectedText, showHUD } from '@raycast/api'
 import '@groqfmt/wasm/dist/wasm-exec.js'
 import { format } from '@groqfmt/wasm'
 import loadGroqfmt from './lib/load-groqfmt'
@@ -8,12 +8,18 @@ const Command = async () => {
   const input = await getSelectedText()
 
   // TODO: Error handling.
-  const groq = format({
+  const result = format({
     input: input ?? '',
     groqfmt,
   })
 
-  await Clipboard.paste(groq.result ?? input ?? '')
+  if (typeof result?.error !== 'undefined') {
+    await showHUD(`Error: ${result.error.message}`)
+    return
+  }
+
+  await Clipboard.paste(result.result ?? input ?? '')
+  await showHUD('Formatted GROQ')
 }
 
 export default Command
